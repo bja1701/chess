718c35aa10f5efe847af8c6e6fb5fe91728a0b47
Brighton Anderson <brightdex17@gmail.com>
2026-02-25 13:26:06 -0700

    skeleton for DataAccess.java in place. not finalized. I might add or takeaway methods in the future.

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,32 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import dataaccess.DataAccessException;
+
+import java.util.Collection;
+
+public interface DataAccess {
+
+    void clear() throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
+
+    void createUser(UserData data) throws DataAccessException;
+
+    int createGame(String gameName)throws DataAccessException;
+
+    GameData getGame(GameData.gameID id) throws DataAccessException;
+
+    Collection<GameData> listGames() throws DataAccessException;
+
+    void updateGame(GameData data) throws DataAccessException;
+
+    void createAuthToken(AuthData data) throws DataAccessException;
+
+    AuthData getAuthToken(String token) throws DataAccessException;
+}
+
+
